799d5f921a6defc06536ccc083c27c6bcfa7086a
Pedro Ramon Farias Lima de Jesus <pedrorfariasljesus@gmail.com>
2026-04-29 10:16:12 -0300

    Update

---
+++ Aula17/objeto.js
@@ -0,0 +1,14 @@
+let pessoa = { // {} cria o objeto
+    nome: "José",
+    sexo: "M",
+    peso: 77.4, 
+    engordar(p) { // é possível criar uma function dentro do objeto 
+        console.log(`${this.nome} engordou ${p}Kg`); 
+        this.peso += p;
+    }
+}
+
+console.log(`${pessoa.nome} pesa ${pessoa.peso.toFixed(2)}Kg`);
+
+pessoa.engordar(3.2);
+console.log(`${pessoa.nome} pesa ${pessoa.peso.toFixed(2)}Kg`);
